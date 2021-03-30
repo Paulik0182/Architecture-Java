1ba6c0aa5f16792762d5c6e464f1e8602d42dc0d
Paveł <Paulik0182@gmail.com>
2021-03-30 17:19:49 +0300

    Lesson3

---
+++ Lesson3/src/com/Lesson3/Main.java
@@ -0,0 +1,59 @@
+package com.Lesson3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SportCar sportCar = new SportCar.Builder("AutoHi").setColor("black").setMaxSpeed(25).build();
+        System.out.println(sportCar.getName());
+        System.out.println(sportCar.getColor());
+        System.out.println(sportCar.getMaxSpeed());
+    }
+}
+
+class SportCar {
+    private final String name;
+    private final String color;
+    private final int maxSpeed;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    private SportCar(Builder builder) {
+        this.name = builder.name;
+        this.color = builder.color;
+        this.maxSpeed = builder.maxSpeed;
+    }
+
+    static class Builder {
+        private final String name;
+        private String color;
+        private int maxSpeed;
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        public Builder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+
+        public SportCar build() {
+            return new SportCar(this);
+        }
+    }
+}
